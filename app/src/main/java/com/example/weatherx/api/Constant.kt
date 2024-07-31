c6c458c69f535a070fd9bbb86fd14ee1374a5610
mohsinshaikh2206 <mohsinshaikh18455@gmail.com>
2024-07-31 23:15:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/weatherx/api/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.weatherx.api
+
+object Constant {
+
+    val apiKey = "929e38cec6604b1fb03114252243107"
+
+}
---
+++ app/src/main/java/com/example/weatherx/api/NetworkResponse.kt
@@ -0,0 +1,12 @@
+package com.example.weatherx.api
+
+import android.adservices.common.AdData
+
+
+sealed class NetworkResponse<out T> {
+    data class Success<out T>(val data : T) : NetworkResponse<T>()
+    data class  Error(val message : String) : NetworkResponse<Nothing>()
+    object  Loading : NetworkResponse<Nothing>()
+
+
+}
